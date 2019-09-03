706b0ab0def6b11f622182df68afbf1b01a073eb
Ashutosh Verma <mrvariable108@gmail.com>
2019-09-03 15:55:31 +0530

    Add record form

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,57 +1,55 @@
-import React,{Fragment} from 'react';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import  SideBar from './components/layout/SideBar';
-import Footer from './components/layout/Footer';
-import Header from './components/layout/Header';
+import SideBar from "./components/layout/SideBar";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import Home from "./components/patient/Home";
 
-import Home from './components/patient/Home';
+import Blog from "./components/patient/Blog";
+import Misc from "./components/patient/Misc";
+import Profile from "./components/patient/Profile";
+import RecordForm from "./components/patient/records/RecordForm";
+import Reminder from "./components/patient/Reminder";
+import SocialPresence from "./components/patient/SocialPresence";
+import SymptomTracker from "./components/patient/SymptomTracker";
 
-import Blog from './components/patient/Blog';
-import Misc from './components/patient/Misc';
-import Profile from './components/patient/Profile';
-//import Record from './components/patient/Record';
-import Reminder from './components/patient/Reminder';
-import SocialPresence from './components/patient/SocialPresence';
-import SymptomTracker from './components/patient/SymptomTracker';
+import AuthState from "./context/auth/AuthState";
 
-import AuthState from './context/auth/AuthState';
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <AuthState>
-<Header/>
-    {/* <div id="App"> */}
+      <Header />
+      {/* <div id="App"> */}
 
       {/* <div id="page-wrap"> */}
       <Router>
- {/* <div className="container"> */}
- 	<Switch> 	
-   <Route exact path='/home' component={Home}/>
+        {/* <div className="container"> */}
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/recordform" component={RecordForm} />
 
-   <Route exact path='/register' component={Register}/>
-  <Route exact path='/login' component={Login}/>
- 	<Route exact path='/' component={Profile}/>
-	<Route exact path='/blog' component={Blog}/> 
- 	<Route exact path='/misc' component={Misc}/>
- 	{/* <Route exact path='/record' component={Record}/> */}
- <Route exact path='/reminder' component={Reminder}/>
- <Route exact path='/socialpresence' component={SocialPresence}/>
-<Route exact path='/symptomtracker' component={SymptomTracker}/>
- 	</Switch>
- 	{/* </div> */}
- </Router>
-    {/* </div> */}
-    {/* </div> */}
-    <Footer/>
-
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Profile} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/misc" component={Misc} />
+          {/* <Route exact path='/record' component={Record}/> */}
+          <Route exact path="/reminder" component={Reminder} />
+          <Route exact path="/socialpresence" component={SocialPresence} />
+          <Route exact path="/symptomtracker" component={SymptomTracker} />
+        </Switch>
+        {/* </div> */}
+      </Router>
+      {/* </div> */}
+      {/* </div> */}
+      <Footer />
     </AuthState>
-
   );
 }
--- website/client/src/components/patient/records/RecordForm.js
+++ website/client/src/components/patient/records/RecordForm.js
@@ -1,26 +1,94 @@
-import React,{Fragment} from 'react'
-import SideBar from '../../layout/SideBar'
+import React, { Fragment } from "react";
+import SideBar from "../../layout/SideBar";
 const RecordForm = () => {
-    return (
-        <Fragment>
-            {/* <h1>This is Record page</h1> */}
-            {/* display of medical records */}
-            {/*
+  return (
+    <Fragment>
+      {/* <h1>This is Record page</h1> */}
+      {/* display of medical records */}
+      {/*
             form submittal 
             date of addition
             purpose of report 
              summary
             submitting pdfs to medical record */}
 
-<div className="page-content">
-    <SideBar/>
-      
+      <div className="page-content">
+        <SideBar />
+        <div className="container-fluid">
+          <div className="card">
+            <div className="card-header header-elements-inline">
+              <h6 className="card-title">Submit Personal Record</h6>
+              {/* <div className="header-elements">
+            <div className="list-icons">
+              <a className="list-icons-item" data-action="collapse" />
+              <a className="list-icons-item" data-action="reload" />
+              <a className="list-icons-item" data-action="remove" />
+            </div>
+          </div> */}
+            </div>
 
-      
+            <div className="card-body" style={{}}>
+              <form action="#">
+                <div className="form-group row">
+                  <label className="col-form-label col-lg-2">Title:</label>
+                  <div className="col-lg-10">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Record Title"
+                    />
+                  </div>
+                </div>
+                {/* <div className="form-group row">
+            <label className="col-form-label col-lg-2">Your password:</label>
+            <div className="col-lg-10">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Your strong password"
+              />
+            </div>
+          </div> */}
+                <div className="form-group row">
+                  <label className="col-form-label col-lg-2">Upload PDF:</label>
+                  <div className="col-lg-10">
+                    <input type="file" className="form-control-file" />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-form-label col-lg-2">
+                    Description:
+                  </label>
+                  <div className="col-lg-10">
+                    <textarea
+                      rows={3}
+                      cols={3}
+                      className="form-control"
+                      placeholder="Record Descreption"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <div className="form-group row mb-0">
+                  <div className="col-lg-10 ml-lg-auto">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <button type="submit" className="btn btn-info">
+                        Cancel
+                      </button>
+                      <button type="submit" className="btn  btn-primary">
+                        Submit
+                        <i className="icon-paperplane ml-2" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      
-            </Fragment>
-    )
-}
+    </Fragment>
+  );
+};
 
 export default RecordForm;
