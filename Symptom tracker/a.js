58e3fe7d4c143f224148bf51672189db003c98b4
ANSH RIYAL <ansh.riyal@gmail.com>
2019-09-11 02:25:28 +0530

    symptom tracker version 1.0

---
+++ Symptom tracker/a.js
@@ -0,0 +1,52 @@
+var std = require("./data.json") //symptoms to diseases
+var dtc = require("./tosort.json") //diseases to count
+
+var x= new Object()
+
+//creating an object containing diseases as keys and values as 0
+for (var i in std){
+	for (var j in std[i]){
+		x[std[i][j]]=0
+	}
+}
+
+//stdin is the input symptoms
+var stdin=[std[0],std[10],std[20],std[2],std[13]]
+//incrementing the values of occurences
+for (var i in stdin){
+	for (var j in stdin[i]){
+		x[stdin[i][j]]+=1
+	}
+}
+
+//to know the no. of symptom matches of each disease
+// for (var i in x){
+// 	console.log(x[i])
+// }
+
+//to predict by symptoms
+var dis= Object.keys(x)
+// console.log(dis)
+var max=0
+var pred=[]
+for (var i in dis){
+	if (x[dis[i]]>max){
+		max=x[dis[i]]
+		var pred=[]
+		pred.push(dis[i])
+	}
+	else if(x[dis[i]]==max){
+		pred.push(dis[i])
+	}
+	else{
+		continue
+	}
+}
+
+var old=Array.from(pred)
+
+//to sort predictions by count
+pred.sort(function(a,b){return dtc[b]-dtc[a]})
+
+console.log(old==pred)
+
