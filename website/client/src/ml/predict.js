cf8c3a3f0bd7c586e89d51b56571eddc81d1cd60
Akash Dhiman <akash.d0407@gmail.com>
2019-09-12 00:14:06 +0530

    implemented final backend for ml

--- website/client/src/ml/predict.js
+++ website/client/src/ml/predict.js
@@ -7,49 +7,62 @@ const word2id = require('./tfjs-model/word2id.json')
 
 const cors = require('cors');
 const express=require("express");
+const data = require("./narendramoditweets.json"); // change to YourTweets.json, which will be constructed by python
 const app = express();
 
 //load the model, need to host the model.json file and access via http
 app.use(cors()); //for cross origin requests
 
 let model;
-async function predict(req,res) {
-  model = await tf.loadLayersModel('http://127.0.0.1:3001/model.json'); //npm http-server /path/to/tfjs-model -p 3000 find a way to host using express.static function
-console.log("model loaded successfully.");
-
-//processing
-  let text = "i am happy"; //this is an example, it will be taken from .txt file sent by ansh...
-  let tokenized_text = text.split(" ");
-  let encoded_samples = [];
-  for (let i = 0; i < tokenized_text.length; i++) {
-    if (word2id[tokenized_text[i]] != undefined) {
-      encoded_samples.push(word2id[tokenized_text[i]])
-    }
-    else {
-      continue;
-    }
-  }
-  encoded_samples = tf.tensor1d(encoded_samples);
-  encoded_samples = encoded_samples.pad([[178 - encoded_samples.size,0]]);
-  encoded_samples = encoded_samples.reshape([1, 178]);
-  const x={
-    hello:'woah',
-    bye:'wow'
-  };
-  const label_probs = model.predict(encoded_samples);
-const values = label_probs.as1D().dataSync();
-const arr = Array.from(values);
-let labels = {
-  surprise: arr[1],
-  anger: arr[0],
-  love: arr[3],
-  joy: arr[2],
-  sadness: arr[4],
-  fear: arr[5]
+var totlabel = {
+  anger     : 0.0,
+  surprise  : 0.0,
+  love      : 0.0,
+  joy       : 0.0,
+  sadness   : 0.0,
+  fear      : 0.0
 };
-console.log(labels);
+var texts = [];
 
-  return res.json(labels);
-}  
+async function predict(req,res) {
+  const address = "http://127.0.0.1:3001/model.json";
+  model = await tf.loadLayersModel(address);
+  console.log("Emotional Analysis Model loaded successfully.")
+
+  //text processing to make it fit for the model.
+  let texts = Object.values(data);
+  for (let i = 0; i < texts.length; i++) {
+    let tokenized_text = texts[i].split(" ");
+    let encoded_samples = [];
+    for (let i = 0; i < tokenized_text.length; i++) {
+      if (word2id[tokenized_text[i]] != undefined) {
+        encoded_samples.push(word2id[tokenized_text[i]])
+      } else {
+        continue;
+      }
+    }
+    encoded_samples = tf.tensor1d(encoded_samples);
+    encoded_samples = encoded_samples.pad([
+      [178 - encoded_samples.size, 0]
+    ]);
+    encoded_samples = encoded_samples.reshape([1, 178]);
+    const label_probs = model.predict(encoded_samples);
+    const values = label_probs.as1D().dataSync();
+    const arr = Array.from(values);
+
+    totlabel["love"]     += arr[0];
+    totlabel["surprise"] += arr[1];
+    totlabel["anger"]    += arr[2];
+    totlabel["joy"]      += arr[3];
+    totlabel["fear"]     += arr[4];
+    totlabel["sadness"]  += arr[5];
+  }
+
+  for (var property in totlabel) {
+    totlabel[property] /= texts.length; //for average
+  }
+    //console.log(totlabel);
+    return res.json(totlabel);
+}
 
 export default predict;
--- website/ml/predict.js
+++ website/ml/predict.js
@@ -1,47 +1,67 @@
+//import axios from 'axios';
+
 const tf = require('@tensorflow/tfjs');
 //const tf = require('@tensorflow/tfjs-node') //breaks at node -v < 10.x.x
 const label2id = require('./tfjs-model/label2id.json')
 const word2id = require('./tfjs-model/word2id.json')
 
+const cors = require('cors');
+const express = require("express");
+const app = express();
+const data = require("./narendramoditweets.json");
+
 //load the model, need to host the model.json file and access via http
-// app.use(express.static(__dirname+"/tfjs-model"));
+app.use(cors()); //for cross origin requests
 
 let model;
-async function predict(req,res) {
-  console.log("before model loads");
-  model = await tf.loadLayersModel('http://127.0.0.1:3001/model.json'); //npm http-server /path/to/tfjs-model -p 3000 find a way to host using express.static function
-console.log("model loaded successfully.");
-
-//processing
-  let text = "i am happy"; //this is an example, it will be taken from .txt file sent by ansh...
-  let tokenized_text = text.split(" ");
-  let encoded_samples = [];
-  for (let i = 0; i < tokenized_text.length; i++) {
-    if (word2id[tokenized_text[i]] != undefined) {
-      encoded_samples.push(word2id[tokenized_text[i]])
-    }
-    else {
-      continue;
-    }
-  }
-  encoded_samples = tf.tensor1d(encoded_samples);
-  encoded_samples = encoded_samples.pad([[178 - encoded_samples.size,0]]);
-  encoded_samples = encoded_samples.reshape([1, 178]);
-  const label_probs = model.predict(encoded_samples); //replace this and send it to chartjs
-  const values = label_probs.as1D().dataSync();
-const arr = Array.from(values);
-let labels = {
-  surprise: arr[1],
-  anger: arr[0],
-  love: arr[3],
-  joy: arr[2],
-  sadness: arr[4],
-  fear: arr[5]
+var totlabel = {
+    anger    : 0.0,
+    surprise : 0.0,
+    love     : 0.0,
+    joy      : 0.0,
+    sadness  : 0.0,
+    fear     : 0.0
 };
-console.log("hello");
-console.log(labels);
-return labels;
-}  
+var texts = [];
 
-module.exports=predict;
-// predict();
+async function predict() {
+    const address = "http://127.0.0.1:3001/model.json"
+    model = await tf.loadLayersModel(address); //npm http-server /path/to/tfjs-model -p 3000 find a way to host using express.static function
+    console.log("model loaded successfully.");
+
+    //processing
+    let texts = Object.values(data);
+    for (let i = 0; i < texts.length; i++) {
+        let tokenized_text = texts[i].split(" ");
+        let encoded_samples = [];
+        for (let i = 0; i < tokenized_text.length; i++) {
+            if (word2id[tokenized_text[i]] != undefined) {
+                encoded_samples.push(word2id[tokenized_text[i]])
+            }
+            else {
+                continue;
+            }
+        }
+        encoded_samples = tf.tensor1d(encoded_samples);
+        encoded_samples = encoded_samples.pad([[178 - encoded_samples.size, 0]]);
+        encoded_samples = encoded_samples.reshape([1, 178]);
+        const label_probs = model.predict(encoded_samples);
+        const values = label_probs.as1D().dataSync();
+        const arr = Array.from(values);
+        
+        totlabel["love"]        += arr[0];
+        totlabel["surprise"]    += arr[1];
+        totlabel["anger"]       += arr[2];
+        totlabel["joy"]         += arr[3];
+        totlabel["fear"]        += arr[4];
+        totlabel["sadness"]     += arr[5];
+};
+    for (var property in totlabel) {
+        totlabel[property] /= texts.length;
+    }
+    console.log(totlabel);
+    //return res.json(labels);
+}
+
+predict();
+//export default predict;
